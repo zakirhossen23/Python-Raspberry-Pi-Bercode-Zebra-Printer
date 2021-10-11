188a89e312491c284de5435c0bea4b777035e2a6
zakirhossen23 <zakirhoss12345@gmail.com>
2021-10-11 11:32:28 +0600

    Settings setup for duplicate finding
    added zebra label printing out
    new barcode format

---
+++ NewProg.py
@@ -0,0 +1,202 @@
+import airtable
+import asyncio
+from datetime import datetime
+import evdev
+import logging
+import os
+import requests
+import sys
+import threading
+import time
+from time import sleep
+import zpl
+
+MY_NAME = 'Rotary3'
+ORDER_ITEM_STATUS = 'pressed'
+
+AIRTABLE_API_KEY = 'keydk7e5EQMNQiDSY' # Insert your API Key
+AIRTABLE_BASE_ID = 'app6ZdMjuZezqltZo' # Insert the Base ID of your working base
+AIRTABLE_TABLE_NAME = 'Pi import' # Insert the name of the table in your working base
+
+last_scan = ''
+prevBarcode = ''
+
+# Key to character table only lists commonly used characters
+keymap = {
+  'KEY_0': u'*',
+  'KEY_1': u'1',
+  'KEY_2': u'2',
+  'KEY_3': u'3',
+  'KEY_4': u'4',
+  'KEY_5': u'5',
+  'KEY_6': u'6',
+  'KEY_7': u'7',
+  'KEY_8': u'8',
+  'KEY_9': u'9',
+  'KEY_A': u'A',
+  'KEY_B': u'B',
+  'KEY_C': u'C',
+  'KEY_D': u'D',
+  'KEY_E': u'E',
+  'KEY_F': u'F',
+  'KEY_G': u'G',
+  'KEY_H': u'H',
+  'KEY_I': u'I',
+  'KEY_J': u'J',
+  'KEY_K': u'K',
+  'KEY_L': u'L',
+  'KEY_M': u'M',
+  'KEY_N': u'N',
+  'KEY_O': u'O',
+  'KEY_P': u'P',
+  'KEY_Q': u'Q',
+  'KEY_R': u'R',
+  'KEY_S': u'S',
+  'KEY_T': u'T',
+  'KEY_U': u'U',
+  'KEY_V': u'V',
+  'KEY_W': u'W',
+  'KEY_X': u'X',
+  'KEY_Y': u'Y',
+  'KEY_Z': u'Z',
+  'KEY_TAB': u'\t',
+  'KEY_SPACE': u' ',
+  'KEY_COMMA': u',',
+  'KEY_SEMICOLON': u';',
+  'KEY_EQUAL': u'=',
+  'KEY_LEFTBRACE': u'[',
+  'KEY_RIGHTBRACE': u']',    
+  'KEY_MINUS': u'-',
+  'KEY_APOSTROPHE': u'\'',
+  'KEY_GRAVE': u'`',
+  'KEY_DOT': u'.',
+  'KEY_SLASH': u'/',
+  'KEY_BACKSLASH': u'\\',
+  'KEY_ENTER': u'\n',
+
+  }
+
+def timeDiff(prevTime, timVal):
+    difference = int(timVal - prevTime)
+    return difference
+
+def createZpl(infoStr):
+    
+    if len(infoStr) < 3:
+        return
+    
+    print("creating Label for Printing", infoStr)
+    #height = 80
+    #image_width = 150
+    
+    height = 25 # 1 inch in mm
+    image_width = 50 # 2 inch in mm
+    
+    l = zpl.Label(450,500)
+    #l = zpl.Label(image_width,height)
+    
+    
+  
+    bercodezpl = "^XA^BY2,1,100^FO40,40^B3^FD"+infoStr+"^FS^XZ"
+
+    print("Got Label as :",bercodezpl)
+  
+    os.system("rm -f /home/pi/Documents/demo.txt")
+    sleep(0.25)
+    zfile = "/home/pi/Documents/demo.txt"
+    f = open(zfile,"w") 
+    f.write(bercodezpl)
+    f.close()
+    Status = False
+    if os.path.isfile(zfile) is False:
+        print("No such File present")
+    else:
+        print("File is present")
+    sleep(0.25)
+    try:
+        os.system("sudo /usr/bin/lp -n 1 -o raw " + zfile)
+        print("Print  success ....")
+        Status = True
+    except Exception as err:
+        print("Cannot print ....")
+    return Status
+
+# Trigger when input is detected
+async def print_events(device):
+  buf = ''
+  global keymap, last_scan
+  async for event in device.async_read_loop():
+    # key_up= 0 key_down= 1 key_hold= 2
+    if event.type == evdev.ecodes.EV_KEY and event.value == 1:
+      kv = evdev.events.KeyEvent(event)
+      # In this modification, map the event to the character table
+      if (kv.scancode == evdev.ecodes.KEY_ENTER):
+        last_scan = buf
+        print("Read Barcode is :", last_scan)
+        barcodeCount=barcodeCount+1
+        buf = ''
+      else:
+        try:
+          buf += keymap.get(kv.keycode)
+        except Exception as err:
+          print("Got Exception")
+          pass
+
+
+def readInput():
+    global last_scan, MY_NAME, pTable , prevBarcode 
+    numpreV=""
+    tbName = "Pi import"
+    barcodeCount = 0
+    #timVal = time.time()
+    #prevTime = time.time()
+    try:
+        
+        while True:
+            
+            if barcodeCount ==1:
+                prevBarcode=last_scan
+            
+                print("prevBarcode is now:", prevBarcode)
+                while True:
+                    if barcodeCount == 2:
+                        if prevBarcode ==last_scan:
+                            createZpl((last_scan.lstrip()).rstrip()) #Newline to fill info in to ZPL var
+                            print("Creating Duplicate Barcode", last_scan)
+                            break
+                        if prevBarcode ==last_scan:
+                            createZpl("Error") 
+                            break
+                barcodeCount=0    
+                import time
+                for i in range(3):
+                    time.sleep(1)
+                    print("\a")    
+                continue        
+
+    except KeyboardInterrupt:
+        logging.debug('Keyboard interrupt')
+    except Exception as err:
+        logging.error(err)
+
+
+if __name__ == '__main__':
+    print("Stating Capturing barcode")
+    import time
+    # for i in range(3):
+    #     time.sleep(3)
+    #     print("\a")
+    thrId = threading.Thread(target=readInput)
+    thrId.start()
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    print('The following devices were found:')
+    for device in devices:
+        print(device.path, device.name, device.phys)
+
+    for device in devices:
+        asyncio.ensure_future(print_events(device))
+
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
+    thrId.join()
+
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Python-Raspberry-Pi-Bercode-Zebra-Printer
- Python Script: Scan Barcode with Scanner & Duplicate of barcodes prints out
+Description of work
+Look for a Python Raspberry Pi Script that Does the following:
+
+-User scans a barcode with a barcode scanner (think barcode on the back of a book)
+
+Then
+
+User Scans another barcode
+
+IF,
+Barcode DOES match, then it prints out a duplicate of that barcode on a label printer that is connected to the Raspberry Pi that will run this script.
+
+IF,
+
+Barcode DOES NOT MATCH, a label with the word "ERROR" gets printed, a 3 second alarm goes off.( alarm will automatically shut off after 3 seconds)
